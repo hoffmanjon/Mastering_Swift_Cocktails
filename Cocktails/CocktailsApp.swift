68764bd0051305e4f8565bb160beae9759b13f9b
Jon Hoffman <hoffman.jon@gmail.com>
2022-06-10 20:35:38 -0400

    Initial Commit

---
+++ Cocktails/CocktailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsApp.swift
+//  Cocktails
+//
+//  Created by Jon Hoffman on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CocktailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cocktails/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cocktails
+//
+//  Created by Jon Hoffman on 6/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
